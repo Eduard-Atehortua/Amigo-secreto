ffd3b8646dcd8ae6a1cb926c236c3c0f783e8dd0
Eduard-Atehortua <eduard.cursos.aprendo@gmail.com>
2025-09-01 13:51:02 -0500

    creando funciones

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus
+//  habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let imoputAmigo = document.getElementById("amigo");
+
+let listaAmigos = []
+
+console.log(imoputAmigo.value);
+ 
+function agregarAmigo() {
+    console.log('Hola')
+}
+
